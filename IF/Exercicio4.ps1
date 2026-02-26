2433f22dffb7c60aca5e61f6bf5369a74a15be62
Sergio Correia <sergio.correia.t0132031@edu.atec.pt>
2026-02-26 11:33:54 +0000

    Exercicios 4 e 5

---
+++ IF/Exercicio4.ps1
@@ -0,0 +1,14 @@
+﻿$saldo = Read-Host "Digite o saldo disponível na sua conta: "
+$cheque = Read-Host "Digite o valor do cheque a descontar: "
+
+$saldo = [int]$saldo
+$cheque = [int]$cheque
+
+if ($saldo -ge $cheque) {
+    $saldoAtual  = $saldo - $cheque
+    Write-Host "Cheque descontado, saldo: $saldo"
+    Write-Host "Fica com um saldo de $saldoAtual."
+
+} else {
+    Write-Host "O valor do cheque é superior ao valor que tem na conta: $saldo"
+} 
---
+++ IF/Exercicio5.ps1
@@ -0,0 +1,41 @@
+﻿$num1 = Read-Host "Digite um número: "
+$num2 = Read-Host "Digite outro número: "
+$num3 = Read-Host "Digite outro número: "
+
+$num1 = [int]$num1
+$num2 = [int]$num2
+$num3 = [int]$num3
+
+if ($num1 -ge $num3 -and $num1 -ge $num2) {
+    $maior = $num1
+    if ($num2 -gt $num3) {
+        $meio = $num2
+        $menor = $num3
+    } else {
+        $meio = $num3
+        $menor = $num2
+    }
+
+} elseif ($num2 -ge $num1 -and $num2 -ge $num3) {
+    $maior = $num2
+    if ($num1 -gt $num3) {
+        $meio = $num1
+        $menor = $num3
+    } else {
+        $meio = $num3
+        $menor = $num1
+    }
+
+} else {
+    $maior = $num3
+    if ($num1 -gt $num2) {
+        $meio = $num1
+        $menor = $num2
+    } else {
+        $meio = $num2
+        $menor = $num1
+    }
+}
+
+Write-Host "Crescente: $menor, $meio, $maior"
+Write-Host "Decrescente: $maior, $meio, $menor" 
