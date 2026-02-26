5810a5f47d96b5a2490f0bc91dfab8e8fe1262fd
Sergio Correia <sergio.correia.t0132031@edu.atec.pt>
2026-02-26 12:11:46 +0000

    Exercicio 6

---
+++ IF/Exercicio6.ps1
@@ -0,0 +1,23 @@
+﻿$valorcompra = Read-Host "Qual foi o valor da compra? "
+$nome = Read-Host "Qual é o nome do cliente? "
+
+$valorcompra = [float]$valorcompra
+$nome = [string]$nome
+
+if ($valorcompra -gt 200 -and $valorcompra -le 500) {
+    $desconto = $valorcompra * 0.15
+    $valorfinal = $valorcompra - $desconto
+
+} elseif ($valorcompra -gt 500) {
+    $desconto = $valorcompra * 0.20
+    $valorfinal = $valorcompra - $desconto
+
+} else {
+    $desconto = $valorcompra * 0.10
+    $valorfinal = $valorcompra - $desconto
+}
+
+Write-Host "Nome: $nome"
+Write-Host "Compra: $valorcompra €"
+Write-Host "Desconto: $desconto €"
+Write-Host "Total a pagar: $valorfinal €"
